439ebc54d189f976e6c92f106821938da5ab8cf2
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-08-21 16:34:26 +0300

    Update IpHack.py

--- IpHack.py
+++ IpHack.py
@@ -40,6 +40,8 @@ try:
         sys.stdout.flush()
         a = lgreen+bold+"[>]"
         b = cyan+bold+"[>]"
+        print (a, "[Status]:", data['status'])
+        print(red+"[+]--------------->"+red)
         print (a, "[Victim]:", data['query'])
         print(red+"[+]--------------->"+red)
         print (b, "[ISP]:", data['isp'])
@@ -50,12 +52,16 @@ try:
         print(red+"[+]--------------->"+red)
         print (a, "[Region]:", data['region'])
         print(red+"[+]--------------->"+red)
+        print (a, "[Country]:", data['country'])
+        print(red+"[+]--------------->"+red)
+        print (a, "[Currency]:", data['currency'])
+        print(red+"[+]--------------->"+red)
         print (b, "[Longitude]:", data['lon'])
         print(red+"[+]--------------->"+red)
         print (a, "[Latitude]:", data['lat'])
         print(red+"[+]--------------->"+red)
         print (b, "[Time zone]:", data['timezone'])
-        print(red+"[+]"+red)
+        print(red+"[+]—————————————————[+]"+red)
         print (a, "[Zip code]:", data['zip'])
         print (" "+yellow)
 
