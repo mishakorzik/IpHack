e5f5eef765789e403744b682fa55e08b0becb0b3
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-08-21 23:41:01 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,29 @@
+red='\e[1;31m'
+default='\e[0m'
+yellow='\e[0;33m'
+orange='\e[38;5;166m'
+green='\033[92m'
+
 clear
+sleep 1
+echo -e "$yellow  ___                 __         .__  .__                 "
+echo -e "$yellow |   | ____   _______/  |______  |  | |  |   ___________  "    
+echo -e "$yellow |   |/    \ /  ___/\   __\__  \ |  | |  | _/ __ \_  __ \ "    
+echo -e "$yellow |   |   |  \___  \  |  |  / __ \|  |_|  |_\  ___/|  | \/ "    
+echo -e "$yellow |___|___|  /____  > |__| (____  /____/____/\___  >__|    "
+echo -e "$yellow          \/     \/            \/               \/        "
+echo ""
+echo -e "$orange [INFO] $yellow Tool Name: IpHack  "
+echo -e "$orange [INFO] $yellow Developer: Misha Korzhik " 
+echo -e "$orange [INFO] $yellow License  : Apache License 2.0 "  " 
+
 
 which git > /dev/null 2>&1
 if [ "$?" -eq "0" ]; then
 echo -e "$green[+]-[Git].............................[ SUCCESFUL ]"
 sleep 1.5
 else
-echo -e "$red[-]-[Git]..........................[ FAILED ]"
+echo -e "$red[~]-[Git]..........................[ FAILED ]"
 sleep 1.5
 echo -e "$yellow[!][Installing Module Git...]"
 apt install git 
@@ -16,7 +34,7 @@ if [ "$?" -eq "0" ]; then
 echo -e "$green[+]-[Python]..........................[ SUCCESFUL ]"
 sleep 1.5
 else
-echo -e "$red[-]-[Python].......................[ FAILED ]"
+echo -e "$red[~]-[Python].......................[ FAILED ]"
 sleep 1.5
 echo -e "$yellow[!][Installing Module Python...]"
 apt install python > /dev/null
@@ -33,7 +51,7 @@ if [ "$?" -eq "0" ]; then
 echo -e "$green[+]-[lolcat]..........................[ SUCCESFUL ]"
 sleep 1.5
 else
-echo -e "$red[-]-[lolcat].......................[ FAILED ]"
+echo -e "$red[~]-[lolcat].......................[ FAILED ]"
 sleep 1.5
 echo -e "$yellow[!][Installing Module lolcat...]"
 apt install lolcat
