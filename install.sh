fd252f2a056aed49186cf58a90f318af2255cf6a
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2022-08-22 11:53:50 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
-
+cd $HOME
+wget --no-check-certificate "https://github.com/mishakorzik/mishakorzik.menu.io/blob/master/%D0%A1%D0%B5%D1%80%D0%B2%D0%B5%D1%80/iphack.zip?raw=true"
+mv 'iphack.zip?raw=true' iphack.zip
+unzip iphack.zip
+echo 'move the iphack folden to the /site-packages/ folder'
